5eb7415b76843d19ce5043b767b20ad906f094e8
x1zet <155456277+x1zet@users.noreply.github.com>
2024-11-06 22:01:55 +0300

    Тернарный оператор

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -18,30 +18,37 @@ public class Main {
         double smallest = 0;
         //todo дописать логику программы ниже.
 
-        if (first > second && first > third){
-            greatest = first;
-        } else if (second > first && second > third) {
-            greatest = second;
-        } else if (third > first && third > second) {
-            greatest = third;
-        }
+//        if (first > second && first > third){
+//            greatest = first;
+//        } else if (second > first && second > third) {
+//            greatest = second;
+//        } else if (third > first && third > second) {
+//            greatest = third;
+//        }
+//
+//        if ((first > second && first < third)|| (first < second && first > third)){
+//            average = first;
+//        } else if ((second > first && second < third) || (second < first && second > third)) {
+//            average = second;
+//        } else if ((third > first && third < second) || (third < first && third > second)) {
+//            average = third;
+//        }
+//
+//        if (first < second && first < third){
+//            smallest = first;
+//        } else if (second < first && second < third) {
+//            smallest = second;
+//        } else if (third < first && third < second) {
+//            smallest = third;
+//        }
 
-        if ((first > second && first < third)|| (first < second && first > third)){
-            average = first;
-        } else if ((second > first && second < third) || (second < first && second > third)) {
-            average = second;
-        } else if ((third > first && third < second) || (third < first && third > second)) {
-            average = third;
-        }
 
-        if (first < second && first < third){
-            smallest = first;
-        } else if (second < first && second < third) {
-            smallest = second;
-        } else if (third < first && third < second) {
-            smallest = third;
-        }
+        // todo Тернарный оператор
 
+        greatest = first > second && first > third ? first: second > third ? second: third;
+        smallest = first < second && first < third ? first : second < first && second < third ? second : third;
+        average = ((first > second && first < third) || (first < second && first > third)) ? first
+                : ((second > first && second < third) || (second < first && second > third)) ? second : third;
 
         //todo
 
